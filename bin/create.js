fb6e2ec9a1000b89d0a57f801985817212666baf
Igor Suvorov <hi@isuvorov.ru>
2017-04-07 08:43:25 +0400

    issue(1): remove es6 icons, improve icon building, svg snake-case bug

--- bin/create.js
+++ bin/create.js
@@ -1,14 +1,19 @@
 var fs = require('fs');
+var fx = require('mkdir-recursive');
 var cheerio = require('cheerio');
 var camelcase = require('camelcase');
 var capitalize = require('capitalize');
 var components = {};
 var types = {};
+var info = require('../icons/info');
 var _ = require('underscore');
 var glob = require('glob');
 var path = require('path');
 var rootDir = path.join(__dirname, '..');
 var attrs = ['xlink:href', 'clip-path', 'fill-opacity', 'fill'];
+function snakeToCamel(s){
+    return s.replace(/(\-[a-z])/g, function(m){return m[1].toUpperCase();});
+}
 var cleanAtrributes = function($el, $) {
     _.each(attrs, function(attr) {
         $el.removeAttr(attr);
@@ -21,7 +26,6 @@ var cleanAtrributes = function($el, $) {
         cleanAtrributes($(el), $);
     });
 };
-
 glob(rootDir + '/icons/*/*.svg', function(err, icons) {
     icons.forEach(function(iconPath){
         var id = path.basename(iconPath, '.svg');
@@ -36,12 +40,21 @@ glob(rootDir + '/icons/*/*.svg', function(err, icons) {
         var folder = iconPath.replace(path.join(rootDir, 'icons') + '/', '').replace( '/' + path.basename(iconPath), '');
         var type = capitalize(camelcase(folder));
         var name = type + capitalize(camelcase(id));
-        var location = iconPath.replace(path.join(rootDir, 'icons'), '').replace('.svg', '.js');
+        // console.log({folder});
+        var location = iconPath.replace(path.join(rootDir, 'icons'), 'es6').replace('.svg', '.js');
+        // console.log({location});
+        fx.mkdirSync(location.split('/').slice(0, -1).join('/'));
         components[name] = location;
         if (!types[folder]) {
             types[folder] = {};
         }
         types[folder][name] = location;
+        // console.log({types});
+        // console.log(iconSvg);
+
+        iconSvg = snakeToCamel(iconSvg)
+        // console.log(iconSvg);
+
         var component = `
 let React = require('react');
 let IconBase = require('react-icon-base');
@@ -56,9 +69,12 @@ export default class ${name} extends React.Component {
     }
 }
 `
+// console.log(svg);
+// console.log(component);
+// process.exit();
 
         fs.writeFileSync(path.join(rootDir, location), component, 'utf-8');
-        console.log(path.join('.', location));
+        // console.log(path.join('.', location));
     });
     _.each(types, function(components, folder) {
         var iconsModule = _.map(components, function(loc, name){
@@ -67,10 +83,9 @@ export default class ${name} extends React.Component {
             loc = "." + loc;
             return `export ${name} from '${loc}';`;
         }).join('\n') + '\n';
-        fs.writeFileSync(path.join(rootDir, folder , 'index.js'), iconsModule, 'utf-8');
-        console.log(path.join('.', folder, 'index.js'));
+        var indexPath = path.join('./es6/', folder, 'index.js');
+        // console.log({indexPath});
+
+        fs.writeFileSync(indexPath, iconsModule, 'utf-8');
     });
-    console.log("IconBase.js");
 });
-
-
